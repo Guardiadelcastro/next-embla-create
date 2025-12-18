d08a2493f99d8d4a25f5ce0109567d372c136a30
Alejandro Castro de la Guardia <guardiadelcastro@gmail.com>
2025-12-18 09:23:24 +0000

    feat: query length

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import type { EmblaOptionsType } from "embla-carousel";
 import EmblaCarousel from "@/components/EmblaCarousel";
+import { useRouter } from "next/router";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,11 +18,16 @@ const SLIDE_COUNT = 5;
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function Home() {
+  const {
+    query: { l },
+  } = useRouter();
+  const queryLength = Number(l);
+  const length = !Number.isNaN(queryLength) ? queryLength : 100;
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
-      {Array.from({ length: 100 }).map((_, index) => (
+      {Array.from({ length }).map((_, index) => (
         <EmblaCarousel key={index} slides={SLIDES} options={OPTIONS} />
       ))}
     </div>
